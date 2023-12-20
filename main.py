adf981999260437b8925370e1698d0b5cc9bacdb
cocosisland <109025296+cocosisland@users.noreply.github.com>
2023-12-20 06:27:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 import spotipy
-import sys
+import os 
 from spotipy import SpotifyClientCredentials, util
-import config
 from get_data import Get_data
 import yaml
 
 
 def credentials_spotify():
-    
-    #client_id = config.client_id
-    #client_secret = config.client_secret
-    
+        
     credentials = yaml.load(open('./credentials.yml'), Loader=yaml.FullLoader)
     client_id = credentials['credentials']['client_id']    
     client_secret = credentials['credentials']['client_secret']
-    
+  
     
     # Credentials to access the Spotify music data
     client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
     sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
     
-    
-    # IF WE CAN ACCESS TO A USER'S PLAYLIST : Credentials to access the playlist's tracks
-    #username = config.username
-    username = credentials['credentials']['username']
-    scope = 'playlist-modify-public'
-    redirect_uri = 'https://example.com'
-    token = util.prompt_for_user_token(username,
-                               scope,
-                               client_id = client_id,
-                               client_secret = client_secret,
-                               redirect_uri = redirect_uri)
-    spt = spotipy.Spotify(auth=token)
-    
-    return sp, spt
+# =============================================================================
+#     # IF WE CAN ACCESS TO A USER'S PLAYLIST : Credentials to access the playlist's tracks
+#     #username = config.username
+#     username = credentials['credentials']['username']
+#     scope = 'playlist-modify-public'
+#     redirect_uri = 'https://example.com'
+#     token = util.prompt_for_user_token(username,
+#                                scope,
+#                                client_id = client_id,
+#                                client_secret = client_secret,
+#                                redirect_uri = redirect_uri)
+#     spt = spotipy.Spotify(auth=token)
+# =============================================================================
+  
+    return sp #, spt
 
+
+# Create a folder specific to the artist in which we save the results file etc.
+def create_dir(path, dirname):
+    if not os.path.exists(dirname):
+        os.mkdir(path + dirname)  
   
 
 
@@ -46,20 +48,24 @@ def credentials_spotify():
 # =============================================================================
 
 if __name__ == "__main__" :
-       
-    sp, spt = credentials_spotify()
     
+    #sp, spt = credentials_spotify()
+    sp = credentials_spotify()
+
     #artist_name = input('Artist name : ')
-    artist_name = 'Whitney Houston'   
+    artist_name = 'Whitney Houston'
+    print(artist_name)
+    
+    create_dir('./', artist_name)
     
     gd = Get_data(artist_name)
 
-    artist_id = gd.get_artist_id(sp)
+    artist_id = gd.get_artist_id(sp)   
     
-    #albums_results = sp.artist_albums(artist_id, limit=1)   
-    
-    albums_ids_list = gd.get_albums_ids(sp, artist_id, 3)
+    albums_ids_list = gd.get_albums_ids(sp, artist_id, 20)
 
-    tracks_id = gd.get_tracks_id(sp, albums_ids_list)
+    tracks_ids_list = gd.get_tracks_ids(sp, albums_ids_list)
 
+    tracks_info_df = gd.get_tracks_info(sp, tracks_ids_list)
+    tracks_info_df.to_csv(os.path.join('./'+artist_name, f"{artist_name}.csv"))
 
